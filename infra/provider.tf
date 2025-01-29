629e6fc42e8ef74db9b6487be6740e67a398afa6
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-29 15:36:23 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "fargate-example/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
